568667ebe18b819180c5e9a20bcdc4d410467261
Nguyen Danh Khuong <nguyendanhkhuong@toilakhuong.local>
2023-01-30 08:51:22 +0700

    fix build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 
-import { Container, Header, Main, Footer, Cards } from "@components";
-
 const Home: React.FC = () => {
-    return (
-        <Container>
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
-        </Container>
-    );
+    return <div>asdfasdf</div>;
 };
 
 export default Home;
