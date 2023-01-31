cf6783dbb1e5fc9d285a704beab63f99c029fc9a
Nguyen Danh Khuong <nguyendanhkhuong@toilakhuong.local>
2023-01-31 16:26:59 +0700

    fix load assets

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const withPlugins = require("next-compose-plugins");
 module.exports = withPlugins([], {
   // imagesPublicPath: "/english-ipa-cheatsheet/_next/static/images/",
   basePath: "/english-ipa-cheatsheet",
-  // assetPrefix: "/english-ipa-cheatsheet",
+  assetPrefix: "/english-ipa-cheatsheet/",
 });
